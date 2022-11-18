06dbedcbd5c9a231943834898978e1588b9f1ee1
Gabriel Veder <gabriel.veder@gmail.com>
2022-11-18 14:35:47 +0100

    My first commit ever. Added some comments to multiple files

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+# list of urls for the 'encyclopedia' app
+
+""" For each desired URL, add an item to the urlpatterns list that contains a call to 
+the path function with two or three arguments: (1) A string representing the URL path, (2) a function from
+views.py that we wish to call when that URL is visited, and (3) (optionally) a name for that path. """
+
 urlpatterns = [
     path("", views.index, name="index")
 ]
