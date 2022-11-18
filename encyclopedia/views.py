021ea17ff0f5e31fcfbb97a6af9266ecd68f742e
Gabriel Veder <gabriel.veder@gmail.com>
2022-11-18 15:04:32 +0100

    added image folder

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from . import util
 
+# views can be thought of as pages the user can see
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
